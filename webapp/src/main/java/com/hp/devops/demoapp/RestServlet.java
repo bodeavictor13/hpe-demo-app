e70b6306e799ef517f0aa976ef350f24295883b3
bodeavictor13 <67367531+bodeavictor13@users.noreply.github.com>
2024-05-30 10:05:19 +0300

    quality story #2097

--- webapp/src/main/java/com/hp/devops/demoapp/RestServlet.java
+++ webapp/src/main/java/com/hp/devops/demoapp/RestServlet.java
@@ -24,6 +24,8 @@ public class RestServlet extends HttpServlet {
 
 
 	
+
+	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String[] pathNodes = Utils.nodify(request.getRequestURI());
